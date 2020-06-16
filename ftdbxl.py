6ec00dc560f8c105d99f04bab363906beef5b2ad
Lars <heuer@semagia.com>
2020-06-17 01:09:57 +0200

    Added count of parts to the construction kits Excel tables

--- ftdbxl.py
+++ ftdbxl.py
@@ -17,7 +17,11 @@ import io
 from operator import itemgetter
 import os
 import json
+from collections import namedtuple
 import xlsxwriter
+
+
+Row = namedtuple('Row', ['id', 'title', 'url', 'article_numbers', 'count'], defaults=(None,))
 
 
 def create_parts_xlfile(db):
@@ -31,46 +35,60 @@ def create_parts_xlfile(db):
     sheet = wb.add_worksheet('Einzelteile')
     bold_style = wb.add_format({'bold': True})
     text_style = wb.add_format({'align': 'top', 'font_color': 'black'})
-    write_parts(sheet, db['parts'].values(), start_row=0, bold_style=bold_style,
-                text_style=text_style)
+    write_header(sheet, ('Bild', 'Beschreibung', 'Artikelnummer (Jahr)'), row=0, header_style=bold_style)
+    write_parts(sheet, (Row(p['id'], p['title'], p['url'], p['article_numbers']) for p in db['parts'].values()),
+                start_row=1, text_style=text_style)
     wb.close()
     with open('excel/einzelteile.xlsx', 'wb') as f:
         f.write(buff.getvalue())
 
 
-def write_parts(sheet, parts, start_row, bold_style, text_style=None):
+def write_header(sheet, headers, row, header_style):
+    """\
+    Writes a header and freezes the row after the header row.
+    
+    :param sheet: Worksheet
+    :param headers: Iterable of strings.
+    :param row: Row index of the header.
+    :param header_style: Format of the header cells.
+    """
+    for idx, header in enumerate(headers):
+        sheet.write(row, idx, header, header_style)
+    sheet.freeze_panes(row + 1, 0)
+
+
+def write_parts(sheet, rows, start_row, text_style=None):
     """\
     Writes parts specification.
 
     :param sheet: Worksheet
-    :param parts: The parts to write (iterable of dicts).
-    :param start_row: Start row for the header.
-    :param bold_style: Format of the header row.
+    :param rows: Iterable of Row instances.
+    :param start_row: Start row index.
     :param text_style: Format of the text cells
     """
-    for idx, header in enumerate(('Bild', 'Beschreibung', 'Artikelnummer (Jahr)')):
-        sheet.write(start_row, idx, header, bold_style)
-    sheet.freeze_panes(start_row + 1, 0)
-    for row_idx, part in enumerate(sorted(parts, key=lambda x: x['title']), start=start_row + 1):
-        img = 'images/parts/{}.png'.format(part['id'])
+    article_nos_col = 2
+    for row_idx, row in enumerate(rows, start=start_row):
+        img = 'images/parts/{}.png'.format(row.id)
         if os.path.isfile(img):
             sheet.insert_image(row_idx, 0, img)
         sheet.set_row(row_idx, 100)
-        title = part['title']
-        sheet.write_formula(row_idx, 1, '=HYPERLINK("{}","{}")'.format(part['url'], title), text_style)
-        sheet.write(row_idx, 2, _artnos(part), text_style)
+        sheet.write_formula(row_idx, 1, '=HYPERLINK("{}","{}")'.format(row.url, row.title), text_style)
+        if row.count:
+            article_nos_col = 3
+            sheet.write(row_idx, 2, row.count, text_style)
+        sheet.write(row_idx, article_nos_col, _artnos(row.article_numbers), text_style)
     sheet.set_column(0, 0, 20)
     sheet.set_column(1, 1, 70)
-    sheet.set_column(2, 2, 22)
+    sheet.set_column(article_nos_col, article_nos_col, 22)
 
 
 def _artnos(dct):
     """\
     Returns the article numbers as string.
 
-    :param dct: A dict with a "article_numbers" key.
+    :param dct: A dict with a artno -> year mapping.
     """
-    return ', '.join(['{} ({})'.format(v or '', k or '') for k, v in sorted(dct['article_numbers'].items(), key=itemgetter(0))]).replace('()', '')
+    return ', '.join(['{} ({})'.format(v or '', k or '') for k, v in sorted(dct.items(), key=itemgetter(0))]).replace('()', '')
 
 
 def create_kit_xlfiles(db):
@@ -104,8 +122,10 @@ def create_kit_xlfile(db, ck):
     if os.path.isfile(img):
         sheet.insert_image(3, 0, img)
         sheet.set_row(3, 180)
-    parts = sorted((part for part in db['parts'].values() if str(part['id']) in ck['parts']), key=lambda x: x['title'])
-    write_parts(sheet, parts, start_row=4, bold_style=bold_style, text_style=text_style)
+    write_header(sheet, ('Bild', 'Beschreibung', 'Anzahl', 'Artikelnummer (Jahr)'), row=4, header_style=bold_style)
+    parts = sorted((part for part in db['parts'].values() if str(part['id']) in ck['parts']), key=itemgetter('title'))
+    write_parts(sheet, (Row(p['id'], p['title'], p['url'], p['article_numbers'], ck['parts'][str(p['id'])]) for p in parts),
+                start_row=5, text_style=text_style)
     wb.close()
     return buff.getvalue()
 
